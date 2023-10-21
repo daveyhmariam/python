3c8f3409486494011837232eaae555dce36817c4
daveyhmariam <daveyhmariam@gmail.com>
2023-10-21 16:00:14 +0300

    zen of python

---
+++ 10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checkes if there is a cycle in a linked list
+ * @list: list to be checked
+ * Return: 0 if there is no cycle 1 otherwise
+*/
+
+int check_cycle(listint_t *list)
+{
+    listint_t *front, *back;
+    back = list;
+    front = list;
+    while ((front->next->next != NULL) && (front->next != NULL) && (front != NULL))
+            {
+				back = back->next;
+				front = front->next->next;
+				if (back == front)
+					return (1);
+            }
+
+	return (0);
+}
